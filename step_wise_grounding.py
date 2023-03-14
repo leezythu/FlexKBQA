93abc10bd7ddcdf914a8d8f88567a10390f6fb06
leezythu <1491547565@qq.com>
2023-03-14 13:07:39 +0000

    update step_wise_grounding

--- step_wise_grounding.py
+++ step_wise_grounding.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 import random
@@ -30,99 +31,47 @@ def filter_valid_rels(results):
             valid_results.append(bind)
     return valid_results
 
+def filter_valid_rets(results):
+    if results["head"]["vars"][0].startswith("rel"):
+        return filter_valid_rels(results)
+    else :
+        return filter_valid_entities(results)
+
 def ground(data,i):
     d = data[i]["Parses"][0]
-    # s = d["Sparql_Tem_First_Stage"]
-#     s = """
-# PREFIX ns: <http://rdf.freebase.com/ns/>
-# SELECT DISTINCT ?rel0
-# WHERE {
-# FILTER (?x != ns:m.0glnq6z)
-# FILTER (!isLiteral(?x) OR lang(?x) = '' OR langMatches(lang(?x), 'en'))
-# ns:m.0glnq6z ?rel0 ?x .
-# }
-    # """
-    # print(s)
-    # exit(0)
-    # sparql.setQuery(s)
-    # sparql.setReturnFormat(JSON)
-    # results = sparql.query().convert()
-    # print(results)
-    # exit(0)
-    # valid_entities = filter_valid_entities(results)
-    # with open(str(i)+"_first_stage_results.json",'w') as f:
-    #     f.write(json.dumps(valid_entities))
-
     valid_entities = json.load(open("grail_entities.json"))
-    entities = random.sample(valid_entities,100)
+    entities = random.sample(valid_entities,10000)
     valid_expansions = []
     for e in entities:
-        second_query = d["Sparql_Tem_Second_Stage"]
-        # for key in e:
-        second_query = second_query.replace("?ent0","ns:"+e[0])
-        print(second_query)
-        sparql.setQuery(second_query)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
+        step_wise_querys = d["step_wise_queries"]
+        replace_info = {}
+        replace_info["?ent0"] = "ns:"+e[0]
+        for query in step_wise_querys:
+            for key in replace_info:
+                if key.startswith("rel"):
+                    query = query.replace("?"+key,"ns:"+replace_info[key].split("/")[-1])
+                else:
+                    query = query.replace(key,replace_info[key])
+            print(query)
+            sparql.setQuery(query)
+            sparql.setReturnFormat(JSON)
+            results = sparql.query().convert()
         # print(results)
-        valid_rels = filter_valid_rels(results)
-        print(valid_rels)
-        exit(0)
-        if len(valid_rels) == 0:
-            continue
-        valid_expansion = valid_rels[0]
-        valid_expansion["ent0"] = {"mid":e[0],"friendly name":e[1]}
-        # print(valid_expansion)
-        valid_expansions.append(valid_expansion)
+            valid_rets = filter_valid_rets(results)
+            print(valid_rets)
+            if len(valid_rets)==0:
+                replace_info = {}
+                break
+            valid_ret = valid_rets[0]
+            for key in valid_ret:
+                replace_info[key] = valid_ret[key]["value"]
+        if replace_info!={}:
+            valid_expansions.append(replace_info)
     with open(str(i)+"_valid_expansions.json",'w') as f:
         f.write(json.dumps(valid_expansions))
     exit(0)
 
-
-# def ground(data,i):
-#     d = data[i]["Parses"][0]
-#     # s = d["Sparql_Tem_Second_Stage"]
-#     s = """
-# PREFIX ns: <http://rdf.freebase.com/ns/>
-# SELECT DISTINCT ?rel0
-# WHERE {
-#     FILTER (?x != ?ent0)
-#     FILTER (!isLiteral(?x) OR lang(?x) = '' OR langMatches(lang(?x), 'en'))
-#     ?ent0 ?rel0 ?x .\n}\n
-#     """
-#     print(s)
-#     sparql.setQuery(s)
-#     sparql.setReturnFormat(JSON)
-#     results = sparql.query().convert()
-#     print(results)
-#     valid_rels = filter_valid_rels(results)
-#     print(valid_rels)
-#     exit(0)
-#     with open(str(i)+"_first_stage_results.json",'w') as f:
-#         f.write(json.dumps(valid_entities))
-#     entities = random.sample(valid_entities,1000)
-#     for e in entities:
-#         second_query = d["Sparql_Tem_Second_Stage"]
-#         for key in e:
-#             second_query = second_query.replace("?"+key,"ns:"+e[key]["value"].split('/')[-1])
-#         print(second_query)
-#         exit(0)
-#         sparql.setQuery(second_query)
-#         sparql.setReturnFormat(JSON)
-#         results = sparql.query().convert()
-#         print(results)
-#         valid_rels = filter_valid_rels(results)
-#         print(valid_rels)
-#         # exit(0)
-#         # with open(str(i)+"_second_stage_results.json",'w') as f:
-#         #     f.write(json.dumps())
-#     exit(0)
-
 if __name__ == '__main__':
     data = json.load(open("sparql_for_prompts.json"))
-    for i in range(1,len(data)):
+    for i in range(2,len(data)):
         ground(data,i)
-    # for d in data:
-    # d = data[0]
-    # ground(d)
-        # exit(0)
