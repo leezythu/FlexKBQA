e80a48734bad578b9750192703bd88b2375b9cb3
leezythu <1491547565@qq.com>
2023-06-17 05:52:47 +0000

    ready for generate kqa data

--- run_turbo_kqa.sh
+++ run_turbo_kqa.sh
@@ -1,6 +1,6 @@
 python ./scripts/annotate_kb_program_turbo_kqa.py --dataset kqa \
---dataset_split kqapro/syn_kqa_sparql \
---save_dir llm_results_turbo_kqa \
+--dataset_split kqapro/syn_kqa_from_ent_sparql \
+--save_dir llm_results_turbo_kqa_from_ent \
 --prompt_file manual_prompts_kqa.txt \
 --output_file llm_kqa \
 --n_parallel_prompts 1 \
