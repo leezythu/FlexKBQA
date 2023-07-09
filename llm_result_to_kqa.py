0d0027939e3023f31342d7ee99f8d34ffabac3fd
leezythu <1491547565@qq.com>
2023-07-09 11:19:02 +0000

    fix 25 prompts fot webqsp

--- llm_result_to_kqa.py
+++ llm_result_to_kqa.py
@@ -1,5 +1,5 @@
 import json
-src_data = json.load(open("llm_results_turbo_kqa/llm_kqa"))
+src_data = json.load(open("llm_results_turbo_kqa_from_ent/llm_kqa_filtered.json"))
 print(len(src_data))
 tgt_data = []
 for key in src_data:
--- llm_results_turbo_kqa_from_ent/filter.py
+++ llm_results_turbo_kqa_from_ent/filter.py
@@ -1,4 +1,28 @@
 import json
+from SPARQLWrapper import SPARQLWrapper, JSON
+import random
+
+SPARQLPATH = "http://localhost:8890/sparql"
+def execute_sparql(s):
+    sparql = SPARQLWrapper(SPARQLPATH)
+    sparql.setQuery(s)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results
+
 data = json.load(open("./llm_kqa"))
-for d in data:
-    if 
+new_data = {}
+for key in data.keys():
+    d = data[key]
+    # print(d)
+    if "\"arg" in  d["generations"] or " arg" in d["ori_data_item"]["question"]:
+        continue
+    #初版产生的时候没有给pred:name之后的entity加上引号
+    sparql = d["ori_data_item"]["sparql"]
+    print(sparql)
+    res = execute_sparql(sparql)
+    print(res)
+    exit(0)
+    new_data[key] = d
+with open("./llm_kqa_filtered.json",'w') as f:
+    f.write(json.dumps(new_data))
