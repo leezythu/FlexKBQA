4a78ef75fce27f8fdd311da0598f150aa918ec93
“lizhenyu” <lizhenyucs@gmail.com>
2023-08-05 12:28:55 +0800

    add kqa test dr

--- run_turbo_for_dr_kqa.sh
+++ run_turbo_for_dr_kqa.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=.
 python ./scripts/annotate_kb_program_turbo_kqa.py --dataset kqa \
---dataset_split Direct_Reasoning/dev_with_none_answer_for_dr \
+--dataset_split Direct_Reasoning/test_with_none_answer_for_dr \
 --save_dir dr_results_50_shot \
 --prompt_file Direct_Reasoning/dr_prompt_for_kqa.txt \
---output_file dr_res_kqa \
+--output_file dr_res_kqa_test \
 --n_parallel_prompts 1 \
 --max_generation_tokens 512 \
 --temperature 0.4 \
